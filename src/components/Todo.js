4ae3ec5f6d17fa64de0a592830a338ce54a92e23
Lyons Ouma <oumalyons26@gmail.com>
2023-05-10 23:11:51 +0300

    added submit function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,9 +3,20 @@ import React, {useState} from 'react'
 const Todo=()=>{
 
     const [name, newName] = useState(" ")
+
+    const sendInputs = ((event)=>{
+        event.preventDefault()
+        console.log({
+            Name : {name}
+        });
+        newName(" ")
+
+    })
+
     return(
-        <form>
+        <form onSubmit={sendInputs}>
            <input value={name} onChange={(e)=>{newName(e.target.value)}}></input>
+           <button type='button'>Send Value</button>
         </form>
     )
 }
