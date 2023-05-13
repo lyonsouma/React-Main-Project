6f73dbf2c4afafaa0d2502b885abc2581462d36b
Lyons Ouma <oumalyons26@gmail.com>
2023-05-13 19:38:34 +0300

    changed navbar colour

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // import logo from './logo.svg';
 import React, {useState} from 'react';
 import './App.css';
-// import NavBar from './components/Navbar';
+import NavBar from './components/Navbar';
 import TodoForm from './components/TodoForm';
 import Display from './components/Display';
 
@@ -10,14 +10,14 @@ import Display from './components/Display';
 
 function App() {
 
-  const [task , addtask]= useState(' ');
+  const [task , addtask]= useState('');
   const [todoList, setTodoList] = useState([]);
 
 
   return (
     <div className="App">
       
-      {/* <NavBar/> */}
+      <NavBar/>
 {
       <div>
          <TodoForm
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -9,12 +9,11 @@ const TodoForm = ({Task, AddTask, TodoList,SetTodoList}) =>{
     const handleSubmit = (event)=>{
       event.preventDefault();
       SetTodoList([...TodoList, Task]);
-      let empty=" ";
-      Task=empty;
+     AddTask('')
     }
 
     return(
-    <Form className='container todo-form-body' onSubmit={handleSubmit}>
+    <Form className='container todo-form-body mt-3' onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Control className='mt-3'  onChange={(e)=>{AddTask(e.target.value)}} placeholder="Today's Task" />
       </Form.Group>
